b6aa475a237318dc2684172cf780ba57aaf00049
hyemin <chm050202@gmail.com>
2022-10-24 12:07:07 +0900

    js 마우스 이벤트 처리

--- 20220907_grid/js/dynamic.js
+++ 20220907_grid/js/dynamic.js
@@ -91,19 +91,36 @@ initButton();
 
 // 급식 API, AJAX로 급식 데이터 가져오기
 // .date-grid-container > .grid-item에 mouseover 이벤트 발생 시 handler 지정하기
-// handler에서 year, month, date 정보 가져와서 url 생성하기
+const handler = (event) => {
+    // handler에서 year, month, date 정보 가져와서 url 생성하기
+    let date = event.target.innerHTML;
+    const KEY = "b72b420005a14d27a0b1def0a75498f2";
+    const ATPT_OFCDC_SC_CODE = "B10";   // 서울특별시 교육청
+    const SD_SCHUL_CODE = "7010569";    // 미림여자정보과학고등학교
+    let MLSV_YMD = `${year}${month.toString().padStart(2, 0)}${date.padStart(2, 0)}`;
+    let url = `https://open.neis.go.kr/hub/mealServiceDietInfo`;
+    url += `?KEY=${KEY}`;
+    url += `&Type=json`;
+    url += `&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`;
+    url += `&SD_SCHUL_CODE=${SD_SCHUL_CODE}`;
+    url += `&MLSV_YMD=${MLSV_YMD}`;
+    console.log(url);
+    getmenuByAPI(url);
+}
+
 // AJAX(Asynchronous JavaScript And XML)로 url 호출하기
 // 응답오면 #breakfast, #lunch, #dinner에 출력하기
-const KEY = "b72b420005a14d27a0b1def0a75498f2";
-const ATPT_OFCDC_SC_CODE = "B10";   // 서울특별시 교육청
-const SD_SCHUL_CODE = "7010569";    // 미림여자정보과학고등학교
-let MLSV_YMD = "20221019";          // 2022년 10월 19일
-let MMEAL_SC_CODE = 2;              // 중식
-let url = `https://open.neis.go.kr/hub/mealServiceDietInfo`;
-url += `?KEY=${KEY}`;
-url += `&Type=json`;
-url += `&ATPT_OFCDC_SC_CODE=${ATPT_OFCDC_SC_CODE}`;
-url += `&SD_SCHUL_CODE=${SD_SCHUL_CODE}`;
-url += `&MLSV_YMD=${MLSV_YMD}`;
-url += `&MMEAL_SC_CODE=${MMEAL_SC_CODE}`;
-console.log(url);
+const getmenuByAPI = (url) => {
+    // XMLHttpRequest 만들기
+    // 요청을 보낼 방식, url, 비동기여부 설정하기
+    // 요청 전송하기
+    // callback
+}
+
+let dateGridContainerDiv = document.getElementsByClassName('date-grid-container')[0];
+let gridItems = dateGridContainerDiv.getElementsByClassName('grid-item');
+for (let gridItem of gridItems) {
+    // console.log(gridItem);
+    gridItem.onclick = handler;
+}
+
